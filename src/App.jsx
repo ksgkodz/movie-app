bc7f1b719b96dd7ed723a757cc28a465059e6b57
ksgkodz <gamer_x420x@proton.me>
2025-07-17 23:38:13 +0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ const App = () => {
     <div className="wrapper">
       <header>
         <img src="./hero.png" alt="Hero Banner"/>
-        <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy without the Hassle</h1>
+        <h1>Search eoifoewfowef <span className='text-gradient'>Movies</span> You'll Enjoy without the Hassle</h1>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {/* <h1 className='text-white'>{searchTerm}</h1> */}
       </header>
